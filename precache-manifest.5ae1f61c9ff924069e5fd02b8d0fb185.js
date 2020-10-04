9a6545c109a193a53a8a3962d208d55899b055ca
kumaraditya_gupta <kumaraditya511@gmail.com>
2020-10-04 17:06:08 +0530

    Updates

---
+++ precache-manifest.5ae1f61c9ff924069e5fd02b8d0fb185.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22434c0c0cbc0b0760f838a4a0bdb023",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "bcca160091aad408e09f",
+    "url": "/react-tutorial/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcca160091aad408e09f",
+    "url": "/react-tutorial/static/js/main.7d7c9805.chunk.js"
+  },
+  {
+    "revision": "d2980c81d16204d1adff",
+    "url": "/react-tutorial/static/js/runtime-main.fba2e450.js"
+  }
+]);
